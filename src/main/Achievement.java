261971778536023c6d1703ba36aae4133db360db
Miguel Avila <miguelaviladeoliveira@gmail.com>
2025-11-11 11:07:04 -0300

    feat: adiciona o código da classe Achievement, com os 'getters' e 'setters'

---
+++ src/main/Achievement.java
@@ -0,0 +1,38 @@
+public class Achievement {
+    private String name;
+    private String description;
+    private int tier; // 0: bronze, 1: silver, 2: gold // ou algo assim
+
+    public Achievement(String name, String description) {
+        this.name        = name;
+        this.description = description;
+        this.tier        = 0;
+    }
+
+    // name
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    // description
+    public String getDescription() {
+        return this.description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    // tier
+    public int getTier() {
+        return this.tier;
+    }
+
+    private void setTier(int tier) {
+        this.tier = tier;
+    }
+}
