413621c1623652cecbd74bbfb23a26add72c1745
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2025-11-19 19:10:11 -0300

    feat: adiciona a classe Review

---
+++ src/main/java/com/mubification/models/Review.java
@@ -0,0 +1,30 @@
+package com.mubification.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    private int rating;
+    private String comment;
+
+    public Review(User user, Movie movie, int rating, String comment) {
+        this.user = user;
+        this.movie = movie;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+}
