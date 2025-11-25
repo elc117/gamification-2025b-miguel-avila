d07fb9dfe435a0512f2a021b38f379b19fb8e1bf
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2025-11-25 19:47:38 -0300

    feat: modifica o main para trabalhar com as rotas dos controllers

--- src/main/java/com/mubification/Main.java
+++ src/main/java/com/mubification/Main.java
@@ -1,49 +1,39 @@
 package com.mubification;
 
-import com.mubification.models.Movie;
-import com.mubification.repositories.MovieRepository;
-import com.mubification.util.Database;
-
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
-
-import java.util.List;
+// Controllers
+import com.mubification.controllers.MovieController;
+import com.mubification.controllers.ReviewController;
+// Repositories
+// Utils
+import com.mubification.util.Database;
 
 public class Main {
 
     public static void main(String[] args) {
 
         String dbUrl = System.getenv("DATABASE_URL");
-
         if (dbUrl == null) {
             System.err.println("ERRO: A variável DATABASE_URL não está definida!");
             System.exit(1);
         }
-
         Database.connect(dbUrl);
 
-        MovieRepository movieRepository = new MovieRepository();
+        ReviewController reviewController = new ReviewController();
+        MovieController movieController   = new MovieController();
 
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add(staticFiles -> {
-                staticFiles.directory = "/public";
+                staticFiles.directory = "/public"; 
                 staticFiles.location = Location.CLASSPATH;
             });
-        }).start(7070);
-
-        app.get("/", ctx -> ctx.redirect("index.html"));
-
-        // GET /movies
-        app.get("/movies", ctx -> {
-            List<Movie> movies = movieRepository.findAll();
-            ctx.json(movies);
         });
+        app.get("/", ctx -> ctx.redirect("/app.html"));
 
-        // POST /movies
-        app.post("/movies", ctx -> {
-            Movie movie = ctx.bodyAsClass(Movie.class);
-            movieRepository.save(movie);
-            ctx.status(201);
-        });
+        reviewController.registerRoutes(app);  
+        movieController.registerRoutes(app);
+
+        app.start(7070);
     }
 }
